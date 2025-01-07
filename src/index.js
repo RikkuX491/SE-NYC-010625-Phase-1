0945dbac743eb7c6fd09d9c1582d6c439c295391
Ricardo Guerra <RikkuG491@gmail.com>
2025-01-07 18:04:20 -0500

    Added solution code for Deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+// Deliverable # 1
+function flatburgerGreeting(){
+    console.log("Welcome to Flatburger!")
+}
+
+// Deliverable # 2
+function printGreeting(greeting){
+    console.log(greeting)
+}
+
+// Deliverable # 3
+function getSum(num1, num2){
+    return num1 + num2
+}
+
+// Deliverable # 4
+const getSumString = function (sum, num1, num2){
+    return `${num1} + ${num2} = ${sum(num1, num2)}`
+}
+
+// Deliverable # 5
+const getAverage = (num1 = 0, num2 = 0) => (num1 + num2) / 2
